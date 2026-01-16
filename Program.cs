9810afdf4d1b8a78544e7bd91aed6a5d64a77bf9
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2026-01-16 15:05:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FailWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kirjuta faili läbi konsooli");
+
+            string filePath = @"C:/Users/opilane/Desktop/WriteToFile.txt";
+            string inputText = Console.ReadLine();
+
+            File.WriteAllText(filePath, inputText);
+        }
+    }
+}
